7319cf7ddc3d675fd44c743c5ca6b229d2040de2
sirdav1d <ddavid.diniz@gmail.com>
2024-10-17 22:19:34 -0300

    feat: :sparkles: ajuste de componente bubble

--- src/components/ui/MyPopup.tsx
+++ src/components/ui/MyPopup.tsx
@@ -2,15 +2,18 @@
 
 'use client';
 
-import { Popup } from '@typebot.io/nextjs';
-import React from 'react';
+import { Bubble } from '@typebot.io/nextjs';
 
 export default function MyPopup() {
 	return (
 		<>
-			<Popup
+			<Bubble
 				typebot='my-typebot-oiffwhn'
-				autoShowDelay={15000}
+				theme={{
+					button: { backgroundColor: '#fee600', iconColor: '#000000' },
+					chatWindow: { backgroundColor: '#163d3c', maxWidth: '50vh' },
+					placement: 'left',
+				}}
 			/>
 		</>
 	);
